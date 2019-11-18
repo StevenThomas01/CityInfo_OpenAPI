64aceeecaefd9e63317d7109b2b8cd0e9a4e73bc
Kiet <kietlo@gmail.com>
2019-11-18 22:10:33 +0000

    Add project files.

---
+++ src/CityInfo.API/Models/CityDto.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Models
+{
+    /// <summary>
+    /// A city with Id, Name, Description, NumberOfPointsOfInterest and 
+    /// PointsOfInterest
+    /// </summary>
+    public class CityDto
+    {
+        /// <summary>
+        /// The Id of the city
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The Name of the City
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The description of the city
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// The number of points of interest for city
+        /// </summary>
+        public int NumberOfPointsOfInterest { get
+            {
+                return PointsOfInterest.Count;
+            }
+        }
+
+        /// <summary>
+        /// Point of inteset for city
+        /// </summary>
+        public ICollection<PointOfInterestDto> PointsOfInterest { get; set; }
+        = new List<PointOfInterestDto>();
+    }
+}
---
+++ src/CityInfo.API/Models/PointOfInterestForCreationDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Models
+{
+    /// <summary>
+    /// Point of interest with name and description.
+    /// </summary>
+    public class PointOfInterestForCreationDto
+    {
+
+        /// <summary>
+        /// The point of interest name.
+        /// </summary>
+        [Required(ErrorMessage ="You should provide a name value.")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The point of interest description.
+        /// </summary>
+        [MaxLength(200)]
+        public string Description { get; set; }
+    }
+}
